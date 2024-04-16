28ebec0adc9a30aa40823557bfc244b81ef5dae2
Fernando Saito <fernandosaito@hotmail.com.br>
2024-04-16 00:30:45 -0400

    finishing the home page

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -14,12 +14,6 @@ export const HomeContainer = styled.main`
     flex-direction: column;
     align-items: center;
     gap: 3.5rem;
-
-    label {
-    }
-
-    input {
-    }
   }
 `
 export const FormContainer = styled.div`
@@ -49,6 +43,32 @@ export const CountdownContainer = styled.div`
   }
 `
 
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
 export const Separator = styled.div`
   padding: 2rem 0;
   color: ${(props) => props.theme['green-500']};
@@ -57,4 +77,32 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
